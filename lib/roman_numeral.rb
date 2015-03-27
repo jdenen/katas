1f820ca40f738dcb486a2e885df53b7006d47518
Johnson Denen <johnson@Johnsons-MacBook-Air.local>
2015-03-26 23:07:00 -0400

    Make test for argument of 1066 pass

--- lib/roman_numeral.rb
+++ lib/roman_numeral.rb
@@ -3,10 +3,22 @@ class RomanNumeral
   NUMERAL_DICTIONARY = {
     "I"   => 1,
     "III" => 3,
-    "IX"  => 9
+    "V"   => 5,
+    "IX"  => 9,
+    "X"   => 10,
+    "L"   => 50,
+    "M"   => 1000
   }
   
   def convert_to_numeral num
-    NUMERAL_DICTIONARY.key num
+    defined_numbers.each_with_object([]) do |n, numeral|
+      next if num.zero?
+      quotient, num = num.divmod(n)
+      numeral << NUMERAL_DICTIONARY.key(n) * quotient
+    end.join
+  end
+
+  def defined_numbers
+    NUMERAL_DICTIONARY.values.reverse
   end
 end
--- spec/roman_numeral_spec.rb
+++ spec/roman_numeral_spec.rb
@@ -2,19 +2,22 @@ require_relative "../lib/roman_numeral"
 
 describe RomanNumeral do
   describe "#convert_to_numeral" do
+    let(:roman){ RomanNumeral.new }
+    
     it "returns I when passed 1" do
-      roman = RomanNumeral.new
       expect(roman.convert_to_numeral 1).to eq "I"
     end
 
     it "returns III when passed 3" do
-      roman = RomanNumeral.new
       expect(roman.convert_to_numeral 3).to eq "III"
     end
 
     it "returns IX when passed 9" do
-      roman = RomanNumeral.new
       expect(roman.convert_to_numeral 9).to eq "IX"
+    end
+
+    it "returns MLXVI when passed 1066" do
+      expect(roman.convert_to_numeral 1066).to eq "MLXVI"
     end
   end
 end
