da50a6eaf04887ca7c6209eae59918e6d32734ff
Johnson Denen <johnson.denen@gmail.com>
2019-02-07 22:04:29 -0500

    Convert arabic to roman

--- elixir/roman_numeral/lib/roman_numeral.ex
+++ elixir/roman_numeral/lib/roman_numeral.ex
@@ -1,2 +1,35 @@
 defmodule RomanNumeral do
+  @lookup %{
+    1 => "I",
+    4 => "IV",
+    5 => "V",
+    9 => "IX",
+    10 => "X",
+    40 => "XL",
+    50 => "L",
+    90 => "XC",
+    100 => "C",
+    400 => "CD",
+    500 => "D",
+    900 => "CM",
+    1000 => "M"
+  }
+
+  def convert(number) do
+    {chars, _} =
+      @lookup
+      |> Map.keys
+      |> Enum.reverse
+      |> Enum.map_reduce(number, &do_convert/2)
+
+    Enum.join(chars)
+  end
+
+  defp do_convert(_, 0), do: {"", 0}
+  defp do_convert(n, acc) do
+    quotient = div(acc, n)
+    remainder = rem(acc, n)
+    {String.duplicate(@lookup[n], quotient), remainder}
+  end
+
 end
--- elixir/roman_numeral/test/roman_numeral_test.exs
+++ elixir/roman_numeral/test/roman_numeral_test.exs
@@ -1,3 +1,24 @@
 defmodule RomanNumeralTest do
   use ExUnit.Case
+  import Checkov
+
+  data_test "converts #{number} to #{numeral}" do
+    assert RomanNumeral.convert(number) == numeral
+
+    where [
+      [:number, :numeral],
+      [1, "I"],
+      [3, "III"],
+      [10, "X"],
+      [14, "XIV"],
+      [45, "XLV"],
+      [59, "LIX"],
+      [190, "CXC"],
+      [1400, "MCD"],
+      [2500, "MMD"],
+      [3900, "MMMCM"],
+      [3999, "MMMCMXCIX"]
+    ]
+  end
+
 end
